af10f521ad68fd5ed8d90679d9409c2037d2245c
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2024-06-14 03:55:24 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
         Ángel David Candelario Rolon
         A01712387
 
-        Dentro del main podemos agregar objetos de clase Libro o Audiolibro, solicitar su información e igual
-        verificar su disponibilidad para luego pedir prestado
+        Dentro del main podemos agregar objetos de clase Libro o Audiolibro,
+        solicitar su información e igual verificar su disponibilidad para
+        luego pedir prestado
 */
+
 #include <iostream>
 #include <string>
 
@@ -14,65 +16,102 @@
 
 using namespace std;
 
-int main(){
-/*
-//PRUEBAS DENTRO DE LA CLASE MATERIAL PARA VER FUNCIONAMIENTO DE LIBROS Y AUDIOLIBROS
-    Libros libro("El Quijote", "Miguel de Cervantes",123);
-    Audiolibros audiolibro("1984", "George Orwell", 321,120);
+void menu(){
+    cout << "\n----> BIBLIOTECA TEC <----\n";
+    cout << "\n--- MENU DE BIBLIOTECA ---\n";
+    cout << "1 - Agregar usuario\n";
+    cout << "2 - Agregar libro\n";
+    cout << "3 - Agregar audiolibro\n";
+    cout << "4 - Consultar materiales\n";
+    cout << "5 - Prestar audiolibro\n";
+    cout << "6 - Prestar libro\n";
+    cout << "7 - Salir\n";
+    cout << "Seleccione una opcion: ";
+}
 
-    //----INTERFAS----
-    cout << "BIENVENIDO A LA BIBLIOTECA VIRTUAL" << endl << "Tenemos disponibles estos materiales" << endl << endl;
-    cout << "Libros:" << endl << libro.get_titulo() << endl << endl;
-    cout << "Audiolibros:" << endl << audiolibro.get_titulo() << endl << endl;
-
-//----SOLICITUDES-----
-    
-    //EJEMPLO LIBROS
-    cout << "Solicitando libro: " << libro.get_titulo() << " verificando disponibilidad ---> " << libro.get_disponibilidad() << endl;
-    libro.prestar();
-    cout << "Ahora el libro:" << libro.get_titulo() << " tiene disponibilidad ---> " << libro.get_disponibilidad() << endl << endl;
-    
-    cout << "EN CASO DE QUE OTRO USUARIO SOLICITA LIBRO" << endl << endl;
-    cout << "Solicitando libro: " << libro.get_titulo() << " verificando disponibilidad ---> " << libro.get_disponibilidad() << endl;
-    libro.prestar();
-    cout << "Ahora el libro:" << libro.get_titulo() << " tiene disponibilidad ---> " << libro.get_disponibilidad() << endl << endl;
-
-    //EJEMPLO AUDIOLIBROS
-    cout << "Solicitando audiolibro: " << audiolibro.get_titulo() << " verificando disponibilidad ---> " << audiolibro.get_disponibilidad() << endl;
-    audiolibro.prestar();
-    cout << "Ahora el audiolibro: " << audiolibro.get_titulo() << " tiene disponibilidad ---> " << audiolibro.get_disponibilidad() << endl << endl;
-
-    cout << "EN CASO DE QUE OTRO USUARIO SOLICITA AUDIOLIBRO" << endl << endl;
-    cout << "Solicitando audiolibro: " << audiolibro.get_titulo() << " verificando disponibilidad ---> " << audiolibro.get_disponibilidad() << endl;
-    audiolibro.prestar();
-    cout << "Ahora el audiolibro: " << audiolibro.get_titulo() << " tiene disponibilidad ---> " << audiolibro.get_disponibilidad() << endl << endl;
-    return 0;
-*/
-
-//MAIN PRINCIPAL DE LA BIBLIOTECA
+int main() {
     Biblioteca biblioteca;
+    string nombre_biblioteca;
 
-    //----INTERFAS----
-    
-//VALORES INICIALES
-    cout << "\n-- AGREGACION DE NUEVOS LIBROS --\n";
+    string ini_usuario;
+            cout << "Ingrese el nombre de un usuario --> ";
+            getline(cin, ini_usuario);
+            biblioteca.agregar_usuario(ini_usuario);
+
+    cout << "\n-- MATERIALES POR DEFAULT --\n";
     biblioteca.agregar_libro("El Quijote", "Miguel de Cervantes", 123);
     biblioteca.agregar_audiolibro("1984", "George Orwell", 321, 120);
-    biblioteca.agregar_usuario("Juan");
+    biblioteca.agregar_usuario("Angel");
     biblioteca.agregar_libro("Nexo","A. Rolon",777);
 
-    cout << "\nBIENVENIDO A LA BIBLIOTECA VIRTUAL" << endl;
-    
-    biblioteca.checar_materiales();
+    while(true){
+        menu();
+        int opcion;
+        cin >> opcion;
 
-    cout << "\n-- EJEMPLO DE PRESTAMOS --\n";
-    biblioteca.entregar_libro(123, "Juan");
-    biblioteca.entregar_audiolibro(321, "Juan");
+        if (opcion == 1) {
+            string nombre_usuario;
+            cout << "Ingrese el nombre del usuario: ";
+            cin.ignore();
+            getline(cin, nombre_usuario);
+            biblioteca.agregar_usuario(nombre_usuario);
+        }
+        else if (opcion == 2) {
+            string titulo, autor;
+            int id;
+            cout << "Ingrese el titulo del libro: ";
+            cin.ignore();
+            getline(cin, titulo);
+            cout << "Ingrese el autor del libro: ";
+            getline(cin, autor);
+            cout << "Ingrese el ID del libro unico: ";
+            cin >> id;
+            biblioteca.agregar_libro(titulo, autor, id);
+        }
+        else if (opcion == 3) {
+            string titulo, autor;
+            int id;
+            float duracion;
+            cout << "Ingrese el titulo del audiolibro: ";
+            cin.ignore();
+            getline(cin, titulo);
+            cout << "Ingrese el autor del audiolibro: ";
+            getline(cin, autor);
+            cout << "Ingrese el ID del audiolibro unico: ";
+            cin >> id;
+            cout << "Ingrese la duracion del audiolibro (en minutos): ";
+            cin >> duracion;
+            biblioteca.agregar_audiolibro(titulo, autor, id, duracion);
+        }
+        else if (opcion == 4) {
+            biblioteca.checar_materiales();
+        }else if (opcion == 5) {
+            int id;
+            string nombre_usuario;
+            cout << "Ingrese el ID del audiolibro a prestar (numeros): ";
+            cin >> id;
+            cin.ignore();
+            cout << "Ingrese el nombre del usuario: ";
+            getline(cin, nombre_usuario);
+            biblioteca.entregar_audiolibro(id, nombre_usuario);
+        }else if (opcion == 6) {
+            int id;
+            string nombre_usuario;
+            cout << "Ingrese el ID del libro a prestar (numeros): ";
+            cin >> id;
+            cin.ignore();
+            cout << "Ingrese el nombre del usuario: ";
+            getline(cin, nombre_usuario);
+            biblioteca.entregar_libro(id, nombre_usuario);
+        }
+        else if (opcion == 7) {
+            cout << "Saliendo de la biblioteca...\n";
+            break;
+        }
+        else {
+            cout << "Opcion no valida. Intente de nuevo.\n";
+        }
+    }
 
-    cout << "\n-- REVISION MATERIALES DESPUES DE SER PRESTADOS --\n";
-    biblioteca.checar_materiales();
-    cout << "\n-- ENTREGA DE LIBROS NO DISPONIBLES --\n";
-    biblioteca.entregar_libro(123,"Juan");
-    biblioteca.entregar_audiolibro(321,"Juan");
-
+    return 0;
 }
