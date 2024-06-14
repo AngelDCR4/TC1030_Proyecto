9d90fab7defed191e121daea20f69b407fd9c082
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2024-06-14 03:54:43 -0600

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -2,7 +2,8 @@
  * Proyecto Biblioteca clase Usuario.h
  * Ángel David Candelario Rolon
  * A01712387
- * Esta clase define el objetos de tipo Usuario para registrarlos y solicitar materiales.
+ * Esta clase define el objetos de tipo Usuario para registrarlos
+ * y solicitar materiales.
  */
 
 #ifndef Usuario_H_
@@ -31,15 +32,26 @@ class Usuarios{
             nombre = nom;
         }
         /*
-        solicitar_material se encarga de mandar a llamar a la funcion prestar de la clase material
-        para hacer entrega del material e imprime un mensaje dependiendo de la disponibilidad del material
+        *   solicitar_material()
+        *
+        *   solicitar_material se encarga de mandar a llamar a la funcion
+        *   prestar de la clase material para hacer entrega del material e 
+        *   imprime un mensaje dependiendo de la disponibilidad del material
+        * 
+        *   @objeto material de la clase Material
+        *   @return funcion prestar
+        *     
         */
+
         void solicitar_material(Material &material){
             if(material.get_disponibilidad()){
-                cout << "El usuario " << nombre << " ha solicitado el material " << material.get_titulo() << endl;
+                cout << "El usuario " << nombre 
+                << " ha solicitado el material " 
+                << material.get_titulo() << endl;
                 material.prestar();
             }else{
-                cout << "Lo sentimos el " << material.get_titulo() << " no esta disponible" << endl;
+                cout << "Lo sentimos el " << material.get_titulo() 
+                << " no esta disponible" << endl;
             }
         }
 };
